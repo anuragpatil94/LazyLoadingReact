2d6f1ac3c88168ccdaf8c327c388e6e2f672a551
Anurag Patil <aspanurag@gmail.com>
2019-08-17 08:16:36 -0400

    Basic React Example

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,14 @@
 import React from "react";
+import { LazyLoadModule } from "./LazyLoadModule";
 
 class App extends React.Component {
   render() {
-    return <div>App</div>;
+    return (
+      <div className="App">
+        <h1>Hello</h1>
+        <LazyLoadModule resolve={() => import("./Module1")} />
+      </div>
+    );
   }
 }
 
